3c42514f1fb6bbc5a3b64192c09b95934eecd89c
Hanan-Ashraf-Mousa <mosah2312@gmail.com>
2025-06-20 04:18:25 +0300

    add doctor and ratings models with JSON serialization; implement patient service for fetching doctors

---
+++ lib/patient_layout/data/services/patient_service.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import '../models/doctor_response.dart';
+
+class PatientService {
+  static final Dio _dio = Dio()
+    ..options = BaseOptions(
+      baseUrl: 'https://healthcare-xi-pied.vercel.app/api/',
+    );
+
+  static Future<List<Doctor>?> getDoctors() async {
+    try {
+      final response = await _dio.get('doctors/doctors');
+      if (response.statusCode == 200) {
+        var json = response.data;
+        if (json['data'] != null && json['data'] is List) {
+          var doctors = (json['data'] as List)
+              .map((doctorJson) => Doctor.fromJson(doctorJson))
+              .toList();
+          return doctors;
+        } else {
+          return [];
+        }
+      } else {
+        return null;
+      }
+    } catch (e) {
+      debugPrint('Error fetching doctors: $e');
+      return null;
+    }
+  }
+
+  static Future<Doctor?> getDoctorById(String id) async {
+    try {
+      final response = await _dio.get('doctors/doctors/$id');
+      if (response.statusCode == 200) {
+        var json = response.data;
+        if (json['data'] != null && json['data'] is Map<String, dynamic>) {
+          var doctor = Doctor.fromJson(json['data']);
+          debugPrint('Fetched doctor: ${doctor.firstName}');
+          return doctor;
+        } else {
+          debugPrint('No valid doctor data found in response');
+          return null;
+        }
+      } else {
+        debugPrint('API request failed with status: ${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      debugPrint('Error fetching doctor by ID: $e');
+      return null;
+    }
+  }
+
+}
