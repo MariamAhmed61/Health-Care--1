be86049cefa1420cdcefbed8a81051ff01d47ac7
Hanan-Ashraf-Mousa <mosah2312@gmail.com>
2025-06-20 06:25:23 +0300

    Refactor main.dart to integrate LanguageCubit and support localization in MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:health_care_app/auth/data/services/auth_service.dart';
 import 'package:health_care_app/auth/pressentation/cubits/auth_cubit/auth_cubit.dart';
 import 'package:health_care_app/core/helper/on_generate_routs.dart';
 import 'package:health_care_app/patient_layout/presentation/cubits/doctor_cubit/doctor_cubit.dart';
+import 'package:health_care_app/patient_layout/presentation/cubits/language_cubit/language_cubit.dart';
+import 'package:health_care_app/patient_layout/presentation/cubits/language_cubit/language_state.dart';
 import 'package:health_care_app/splash/views/splash_screen.dart';
 
 void main() {
@@ -18,13 +21,28 @@ class HealthCare extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => DoctorCubit()..fetchDoctors()),
-        BlocProvider(create: (context)=>AuthCubit(AuthService()))
+        BlocProvider(create: (context) => AuthCubit(AuthService())),
+        BlocProvider(create: (context) => LanguageCubit())
       ],
-      child: const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          onGenerateRoute: onGenerateRoute,
-          initialRoute: SplashScreen.routeName,
-          ),
+      child: BlocBuilder<LanguageCubit, LanguageState>(
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            onGenerateRoute: onGenerateRoute,
+            initialRoute: SplashScreen.routeName,
+            locale: Locale(state.languageCode),
+            supportedLocales: const [
+              Locale('en', ''), // English
+              Locale('ar', ''), // Arabic
+            ],
+            localizationsDelegates: const [
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+          );
+        },
+      ),
     );
   }
 }
